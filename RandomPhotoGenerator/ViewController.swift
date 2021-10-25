d059205739ba5572c6707c52f768f902bb612cde
estheroidss <30939630+estheroidss@users.noreply.github.com>
2021-10-25 12:53:55 -0300

    Initial Commit

---
+++ RandomPhotoGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotoGenerator
+//
+//  Created by Esther Laudier on 25/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
